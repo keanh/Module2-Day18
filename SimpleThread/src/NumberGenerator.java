b8c5a8b59d9a9939a02c27be2b1bc80a4e01e9e7
keanh <keanh275@gmail.com>
2020-03-09 14:54:10 +0700

    fix

--- SimpleThread/src/NumberGenerator.java
+++ SimpleThread/src/NumberGenerator.java
@@ -11,12 +11,17 @@ public class NumberGenerator implements Runnable {
                 e.printStackTrace();
             }
             System.out.println(i + " ");
+            System.out.println("Độ ưu tiên " + Thread.currentThread().getPriority() );
         }
     }
 
     public static void main(String[] args) {
         NumberGenerator numberGenerator = new NumberGenerator();
-        Thread thread = new Thread(numberGenerator);
-        thread.start();
+        Thread thread1 = new Thread(numberGenerator);
+        Thread thread2 = new Thread(numberGenerator);
+        thread1.setPriority(Thread.MAX_PRIORITY);
+        thread2.setPriority(Thread.MIN_PRIORITY);
+        thread1.start();
+        thread2.start();
     }
 }
